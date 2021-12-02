e78ae2e96e6793ea65d62e824c3f3254d37a9a12
khalidhersi <cipherpolfoundation@gmail.com>
2021-12-02 14:48:40 +0000

    counter completed + reset button

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Ticket.scss";
 import Employee from "../Employee/Employee";
 
 
 const Ticket = (props) => {
-let counter = 0;
 const {id} = props;
+const [counter, setCounter] = useState(0)
+
+const handleIncrement = () => {
+    setCounter(counter + 1)
+}
+
+const handleDecrement = () => {
+    if(counter >0) {
+        setCounter(counter - 1)
+    }    
+}
+
+const handleReset = () => {
+     setCounter(0)
+}
+
+console.log(counter)
 
     return <div className="ticket__border">
         <h3 className="ticket__title">Ticket</h3>
         <div className="ticket__counter">
             <h5 className="ticket__counter-title">Counter</h5>
-            <p className="counter">{counter += id}</p>
-            <p className="minus">-</p>
-            <p className="plus">+</p>
+            <p className="counter">{0 + counter}</p>
+            <div className="buttons">                
+                <button className="decrement" onClick={handleDecrement}>-</button>
+                <button className="increment" onClick={handleIncrement}>+</button>
+            </div>
+            <button className="reset" onClick={handleReset}>Reset</button>
         </div>
         <Employee />
     </div>
